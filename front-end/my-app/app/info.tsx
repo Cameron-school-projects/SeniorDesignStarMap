746aeed26f927947c6bb336feccaad7a1dbd87c9
Cameron Beck <cameron@pixelthin.com>
2024-04-25 16:29:45 -0500

    added text to info dialoge

--- front-end/my-app/app/info.tsx
+++ front-end/my-app/app/info.tsx
@@ -37,7 +37,20 @@ const InfoDialog: React.FC = () => {
           <DialogTitle>Instructions</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              So this is where we can put instructions for user to implement the map it stuff
+             In order to generate a star map, this application needs 4 things:<br />
+             1) a latitude, in the format of DDD-MM-SS.SS[N|S], where D is degrees, M is minutes, and S is seconds,<br />
+             2) a longitude, in the format of DDD-MM-SS.SS[E|W], where D is degrees, M is minutes, and S is seconds,<br />
+             3) a clock time, entered in the time picker, or with the clock infographic,<br />
+             4) and a date, entered through the calender picker.<br />
+             <br />
+             Once these are entered, pressing the map it! button will generate a star map for your viewing pleasure.<br /><br />
+             The "Toggle Labels" button allows you to change if stars, constellations, and planets have labels. Note: this will also change what version is downloaded.<br />
+             Lastly, the download starmap button downloads the starmap onto your personal computer. <br />
+             <br />
+             Enjoy!
+             <br />
+             -The Astronomicon
+
             </DialogContentText>
           </DialogContent>
           <DialogActions>
